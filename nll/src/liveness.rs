fcf35880c1c3ca59b6c9780872b2764c4e4623d0
Niko Matsakis <niko@alum.mit.edu>
2017-02-20 20:50:03 -0500

    refactor how liveness works to use callback

--- nll/src/liveness.rs
+++ nll/src/liveness.rs
@@ -1,7 +1,7 @@
 use graph::{BasicBlockIndex, FuncGraph};
-use env::Environment;
+use env::{Environment, Point};
 use graph_algorithms::Graph;
-use graph_algorithms::bit_set::BitSet;
+use graph_algorithms::bit_set::{BitBuf, BitSet, BitSlice};
 use nll_repr::repr;
 use std::collections::HashMap;
 
@@ -18,54 +18,62 @@ impl Liveness {
                                                .cloned()
                                                .zip(0..)
                                                .collect();
-        let liveness = compute(env, &var_bits);
-        Liveness { var_bits, liveness }
+        let liveness = BitSet::new(env.graph, var_bits.len());
+        let mut this = Liveness { var_bits, liveness };
+        this.compute(env);
+        this
     }
 
     pub fn live_on_entry(&self, v: repr::Variable, b: BasicBlockIndex) -> bool {
         let bit = self.var_bits[&v];
         self.liveness.bits(b).get(bit)
     }
-}
 
-fn compute(env: &Environment,
-           var_bits: &HashMap<repr::Variable, usize>)
-           -> BitSet<FuncGraph> {
-    let mut liveness = BitSet::new(env.graph, var_bits.len());
-    let mut bits = liveness.empty_buf();
+    fn compute(&mut self, env: &Environment) {
+        let mut bits = self.liveness.empty_buf();
+        let mut changed = true;
+        while changed {
+            changed = false;
 
-    let mut changed = true;
-    while changed {
-        changed = false;
-
-        for &block in &env.reverse_post_order {
-            bits.clear();
-
-            // everything live in a successor is live at the exit of the block
-            for succ in env.graph.successors(block) {
-                bits.set_from(liveness.bits(succ));
+            for &block in &env.reverse_post_order {
+                self.simulate_block(env, &mut bits, block, |_p, _a, _s| ());
+                changed |= self.liveness.insert_bits_from_slice(block, bits.as_slice());
             }
-
-            // walk backwards through the actions
-            for action in env.graph.block_data(block).actions.iter().rev() {
-                let (def_var, use_var) = action.def_use();
-
-                // anything we write to is no longer live
-                for v in def_var {
-                    bits.kill(var_bits[&v]);
-                }
-
-                // anything we read from, we make live
-                for v in use_var {
-                    bits.set(var_bits[&v]);
-                }
-            }
-
-            changed |= liveness.insert_bits_from_slice(block, bits.as_slice());
         }
     }
 
-    liveness
+    fn simulate_block<CB>(&mut self,
+                          env: &Environment,
+                          buf: &mut BitBuf,
+                          block: BasicBlockIndex,
+                          mut callback: CB)
+        where CB: FnMut(Point, &repr::Action, BitSlice)
+    {
+        buf.clear();
+
+        // everything live in a successor is live at the exit of the block
+        for succ in env.graph.successors(block) {
+            buf.set_from(self.liveness.bits(succ));
+        }
+
+        // walk backwards through the actions
+        for (index, action) in env.graph.block_data(block).actions.iter().enumerate().rev() {
+            let (def_var, use_var) = action.def_use();
+
+            // anything we write to is no longer live
+            for v in def_var {
+                buf.kill(self.var_bits[&v]);
+            }
+
+            // anything we read from, we make live
+            for v in use_var {
+                buf.set(self.var_bits[&v]);
+            }
+
+            let point = Point { block, action: index };
+            callback(point, action, buf.as_slice());
+        }
+    }
 }
 
 trait UseDefs {
